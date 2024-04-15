cca36bb5dc3ad21a9dc9f2cbcaea343c6becc20c
simon31fu <ximeng1@ualberta.ca>
2024-04-14 19:54:43 -0600

    test

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -34,7 +34,7 @@ golem::add_shinyserver_file()
 ## Docker ----
 ## If you want to deploy via a generic Dockerfile
 golem::add_dockerfile_with_renv()
-
+golem::add_dockerfile()
 ## If you want to deploy to ShinyProxy
 golem::add_dockerfile_with_renv_shinyproxy()
 
@@ -57,3 +57,4 @@ rsconnect::deployApp(
   lint = FALSE,
   forceUpdate = TRUE
 )
+
