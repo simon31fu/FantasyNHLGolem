7db8a51a833553cf4d37fddacb55bec59c1b0d6d
simon31fu <ximeng1@ualberta.ca>
2024-04-14 19:15:05 -0600

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -21,7 +21,7 @@
 golem::fill_desc(
   pkg_name = "testonetwo", # The Name of the package containing the App
   pkg_title = "test12", # The Title of the package containing the App
-  pkg_description = "test for golem app submission", # The Description of the package containing the App
+  pkg_description = "Test for golem app submission. Updated as import progresses.", # The Description of the package containing the App
   author_first_name = "Simon", # Your First Name
   author_last_name = "Fu", # Your Last Name
   author_email = "ximeng1@ualberta.ca", # Your Email
